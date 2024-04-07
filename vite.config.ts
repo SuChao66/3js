71db85308782140359c436def192b73b64ba699e
SuChao66 <suchao12138@163.com>
2024-04-07 20:59:05 +0800

    删除无用代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+// 导入类型
+import type { UserConfig, ConfigEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
